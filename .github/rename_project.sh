241e47302a7c9601b33f34278a3abd65a7359679
verganas <verganas@users.noreply.github.com>
2023-03-05 07:46:52 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="verganas"
+original_name="mil_test"
+original_urlname="mil_test"
+original_description="Awesome mil_test created by verganas"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv mil_test $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
